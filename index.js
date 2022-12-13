c7410a85c7fd888cf1acaddd2bfb40a415029018
봄수 <37897508+GoBeromsu@users.noreply.github.com>
2022-12-13 17:07:12 +0900

    Update : problem2

--- index.js
+++ index.js
@@ -14,17 +14,28 @@ problem6();
 
 function problem1() {
     //TODO: 1. users의 평균나이를 구하시오
-    console.log(`평균나이: ${users.reduce((prev, cur) => prev + cur.age, 0)/users.length}`);
+    console.log(
+        `평균나이: ${
+            users.reduce((prev, cur) => prev + cur.age, 0) / users.length
+        }`
+    );
 }
 
 function problem2() {
     //TODO: 2. 24살이상인 user를 출력하시오
-    console.log(users.filter((user) => user.age >= 24));
+    console.log(users.filter(({ age }) => age >= 24));
 }
 function problem3() {
     //TODO: 3. users의 이름[나이/성별]로 출력하시오
     //ex) 민준[24/남], 하윤[23/여]
-    console.log(users.map((user) => `${user.name}[${user.age}/${user.gender == "male" ? "남" : "여"}]`));
+    console.log(
+        users.map(
+            (user) =>
+                `${user.name}[${user.age}/${
+                    user.gender == "male" ? "남" : "여"
+                }]`
+        )
+    );
 }
 
 function problem4() {
@@ -37,5 +48,9 @@ function problem5() {
 }
 function problem6() {
     //TODO: 6. users를 생일(년도오름차순)으로 정렬하시오
-    console.log(users.sort((a, b) => new Date(a.birth).getTime() - new Date(b.birth).getTime()));
+    console.log(
+        users.sort(
+            (a, b) => new Date(a.birth).getTime() - new Date(b.birth).getTime()
+        )
+    );
 }
